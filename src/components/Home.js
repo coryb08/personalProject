281ddfe688e6f62adec5260536d47bc5704c192a
github username <github email address>
2018-03-01 12:29:42 -0500

    added stackoverflow and email

--- src/components/Home.js
+++ src/components/Home.js
@@ -17,23 +17,33 @@ const Home = props => {
       <div className="message">
         My website is in the works. <br />In the mean time here are some links.
       </div>
-      <a
-        target="_blank"
-        href="https://www.facebook.com/corydbaker"
-        className="facebook"
-      />
+      <div className="links">
+        <a
+          target="_blank"
+          href="https://www.facebook.com/corydbaker"
+          className="facebook"
+        />
 
-      <a
-        target="_blank"
-        href="https://medium.com/@corybaker_54290"
-        className="medium"
-      />
+        <a
+          target="_blank"
+          href="https://medium.com/@corybaker_54290"
+          className="medium"
+        />
 
-      <a
-        target="_blank"
-        href="https://www.linkedin.com/in/cory-baker-5a533895/"
-        className="linkedin"
-      />
+        <a
+          target="_blank"
+          href="https://www.linkedin.com/in/cory-baker-5a533895/"
+          className="linkedin"
+        />
+
+        <a href="mailto:douglasb2008@gmail.com" className="email" />
+
+        <a
+          target="_blank"
+          href="https://stackoverflow.com/users/9094578/cory-baker?tab=profile"
+          className="stackOverflow"
+        />
+      </div>
     </div>
   )
 }
